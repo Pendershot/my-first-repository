3bc6897b439a8bc3cecfc1045e8d8c6dd543dc18
Paul Jertberg <paul.jertberg@gmail.com>
2015-05-30 15:31:03 +0000

    Second push; completed another exercise; testing git login timeout

--- flow.rb
+++ flow.rb
@@ -1,5 +1,22 @@
 #!/usr/bin/ruby
 
+# Exercise 1
+
+#False
+(32 * 4) >= 129
+
+#False
+false != !true
+
+#False
+true == 4
+
+#True
+false == (847 == '874')
+
+#True
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false
+
 
 # Exercise 3
 
